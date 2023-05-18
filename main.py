9d4ac6754cda41ff76effae2299f2622a8cd5130
Prathyusha Guduru <prathyushaguduru28@gmail.com>
2023-05-18 16:34:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 
+from starlette.middleware.cors import CORSMiddleware
+
 from app.api.endpoints.api import router
 from app.core.config import settings
 from app.core.exceptions import ExceptionSet, exception_handler
@@ -7,6 +9,14 @@ from app.core.logging import setup_requests_logging, setup_sql_logging
 
 app = FastAPI(title="slm", version=1)
 app.include_router(router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if exception_handler:
     for exception in ExceptionSet:
@@ -17,4 +27,4 @@ if settings.SHOW_SQL_ALCHEMY_QUERIES:
     setup_sql_logging()
 
 if settings.SHOW_OUTGOING_REQUESTS:
-    setup_requests_logging()
+    setup_requests_logging(),
